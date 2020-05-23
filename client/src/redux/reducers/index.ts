ea8ee9cb11e9c3dcae3cb2119a642d966642bc51
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-05-24 00:11:01 +0530

    Setup redux config

---
+++ client/src/redux/reducers/index.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+// TODO: Add a StoreState for combine reducers
+
+const rootReducer = combineReducers({
+	artworks: () => [],
+});
+
+export default rootReducer;
