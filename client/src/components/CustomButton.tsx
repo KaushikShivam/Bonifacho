3c7c5d7e0c3b427b90365b94f5456b7fcce66421
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-05-23 18:42:07 +0530

    Create Custom button

---
+++ client/src/components/CustomButton.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react';
+
+import { ButtonContainer } from './../styles/CustomButton.styles';
+
+interface AppProps {
+	children: ReactNode;
+	backgroundColor: string;
+	fontSize: string;
+	handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const CustomButton = ({ children, handleClick, ...otherProps }: AppProps) => {
+	return (
+		<ButtonContainer {...otherProps} onClick={handleClick}>
+			{children}
+		</ButtonContainer>
+	);
+};
+
+export default Button;
---
+++ client/src/styles/CustomButton.styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+interface ButtonProps {
+	backgroundColor: string;
+	fontSize: string;
+}
+
+export const ButtonContainer = styled.button<ButtonProps>`
+	font-family: 'Playfair Display', 'Courier New';
+	font-weight: bold;
+	letter-spacing: 2px;
+	color: white;
+	border: none;
+	cursor: pointer;
+	transition: all 0.2s;
+	background-color: ${({ backgroundColor }) => backgroundColor};
+	font-size: ${({ fontSize }) => fontSize};
+
+	&:focus {
+		outline: none;
+	}
+
+	&:hover {
+		filter: brightness(90%);
+	}
+`;
