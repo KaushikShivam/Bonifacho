cce101f510277e5d11f8dc0d4139ff959624e8bf
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-05-23 23:43:16 +0530

    Finish implimenting ArtowrkBig

--- client/src/components/ArtworkBog.tsx
+++ client/src/components/ArtworkBog.tsx
@@ -1,30 +1,48 @@
 import React from 'react';
-import './ArtworkBig.scss';
 
-import CustomButton from './../CustomButton/CustomButton';
+import CustomButton from './CustomButton';
 
-const ArtworkBig = ({ name, artist, image }) => {
+import {
+	ImageContainer,
+	Content,
+	Title,
+	Author,
+	Detail,
+} from './../styles/ArtworkBig.styles';
+
+interface AppProps {
+	name: string;
+	artist: {
+		name: string;
+	};
+	image: string;
+}
+
+// TODO: Handle Click handler
+
+const ArtworkBig = ({ name, artist, image }: AppProps) => {
 	return (
-		<div className='ArtworkBig'>
-			<div className='ArtworkBig-img'>
+		<div>
+			<ImageContainer>
 				<img src={image} alt={name} />
-			</div>
-			<div className='ArtworkBig-content'>
-				<div className='ArtworkBig-info'>
-					<h3 className='ArtworkBig-title'>{name}</h3>
-					<p className='ArtworkBig-author'>{artist.name}</p>
+			</ImageContainer>
+			<Content>
+				<div>
+					<Title>{name}</Title>
+					<Author>{artist.name}</Author>
 				</div>
 				<div className='ArtworkBig-detail'>
 					<CustomButton
 						backgroundColor={`#AB9B8A`}
 						padding='5px 7px'
 						fontSize='1rem'
+						handleClick={() => {}}
 					>
 						SHOP NOW
 					</CustomButton>
 					<p className='ArtworkBig-price'>$3,490</p>
 				</div>
-			</div>
+			</Content>
 		</div>
 	);
 };
