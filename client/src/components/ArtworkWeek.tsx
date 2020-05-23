5470aa335ad40c271d44696903a1523882b00ad5
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-05-23 23:09:06 +0530

    Add ArtworkWeek container

--- client/src/components/ArtworkWeek.tsx
+++ client/src/components/ArtworkWeek.tsx
@@ -1,36 +1,28 @@
 import React from 'react';
-// import { connect } from 'react-redux';
-// import './ArtworkWeek.scss';
 
-// import ArtworkBig from './../../../../components/ArtworkBig/ArtworkBig';
-// import CustomLink from './../../../../components/CustomLink/CustomLink';
+import CustomLink from './CustomLink';
 
-// import { getWeeklyArtworks } from './../../../../redux/actions/artwork';
+import {
+	Container,
+	Title,
+	Content,
+	LinkContainer,
+} from './../styles/ArtworkWeek.styles';
 
-// const ArtworkWeek = ({ artworkWeek, getWeeklyArtworks }) => {
-// 	useEffect(() => {
-// 		getWeeklyArtworks();
-// 	}, [getWeeklyArtworks]);
+const ArtworkWeek = () => {
+	return (
+		<Container>
+			<Title>ARTWORKS OF THE WEEK</Title>
+			<Content>
+				{/* {artworkWeek.map((artwork) => (
+					<ArtworkBig key={artwork.id} {...artwork} />
+				))} */}
+			</Content>
+			<LinkContainer>
+				<CustomLink>SHOP ALL ARTWORKS</CustomLink>
+			</LinkContainer>
+		</Container>
+	);
+};
 
-// 	return (
-// 		<div className='ArtworkWeek'>
-// 			<h2 className='ArtworkWeek-title'>ARTWORKS OF THE WEEK</h2>
-// 			<div className='ArtworkWeek-content'>
-// 				{artworkWeek.map((artwork) => (
-// 					<ArtworkBig key={artwork.id} {...artwork} />
-// 				))}
-// 			</div>
-// 			<div className='ArtworkWeek-link'>
-// 				<CustomLink>SHOP ALL ARTWORKS</CustomLink>
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// const mapStateToProps = (state) => ({ artworkWeek: state.artwork.artworkWeek });
-
-// const mapdispatchToProps = (dispatch) => ({
-// 	getWeeklyArtworks: () => dispatch(getWeeklyArtworks()),
-// });
-
-// export default connect(mapStateToProps, mapdispatchToProps)(ArtworkWeek);
+export default ArtworkWeek;
