b8ef0ee53cc30ef5e7bc35ba59ea565fcb004676
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-05-23 18:16:01 +0530

    Create styled components for Hero

---
+++ client/src/styles/Hero.styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Hero = styled.div`
+	height: 50rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	.btns > * {
+		margin: 0 1rem;
+	}
+`;
+
+export const Title = styled.h1`
+	font-size: 6.2rem;
+	font-family: 'Montserrat', 'Courier New';
+	color: white;
+	letter-spacing: 4px;
+	font-weight: bold;
+	margin-bottom: 4rem;
+`;
+
+export const SubTitle = styled.h3`
+	color: rgba(255, 255, 255, 0.85);
+	font-size: 2rem;
+	font-family: 'Playfair Display', 'Courier New';
+	margin-bottom: 1.2rem;
+	letter-spacing: 3px;
+	font-weight: bold;
+`;
